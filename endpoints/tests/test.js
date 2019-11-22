8ac03de6b67282b046fb5ba8f0f4fd7578159470
Darshan <darshanmarathe@gmail.com>
2019-11-22 19:46:47 +0530

    added code for super test

--- endpoints/tests/test.js
+++ endpoints/tests/test.js
@@ -2,7 +2,9 @@ const tape = require('tape')
 const mixedTape = require('mixed-tape')
 const test = mixedTape(tape);
 
-
+const app = require("../app"); // Link to your server file
+const supertest = require("supertest");
+const request = supertest(app);
 
 
 const axios  = require("axios");
@@ -11,27 +13,22 @@ var result = {};
 
 const path = 'http://localhost:3000/'
 
-test('setup', async(t) => {
-    console.log('setup')
-    result = await axios.get(`${path}users`);
-    t.end();
+test('Make Sure that we can call Express code', async t => {
+   const data = await  request.get('/users');
+
+   t.equal(data.body.length , 3);
+    t.end();   
 });
 
-test('Make sure that you can make AJAX call', async t => {
-    console.log('result')
-    t.equal(result.length , 3)   
-});
+// test('Make sure that you can make AJAX call', async t => {
+//     result = await axios.get(`${path}users`); 
+//     t.equal(result.data.length , 3);
+//     t.end();   
+// });
 
 
-test('Make sure that you can make AJAX call', async t => {
-    t.equal(result.length , 3)   
-});
+// test('Make sure that you can make AJAX call', async t => {
+//     t.equal(result.data.length , 3);
+//     t.equal(result.length , 3)   
+// });
 
-
-
-test('teardown', async(t) => {
-  result = {};
-  console.log('tareing down');
-  
-    t.end();
-});
